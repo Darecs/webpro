4728c0cd1767216bcdef4d509d8873fa9eb11760
Julian Kuehn <jkuehn@xevit.intra>
2019-06-12 16:40:54 +0200

    New Project Structure

--- webpro/webpro/settings/base.py
+++ webpro/webpro/settings/base.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+PREREQ_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+PROJECT_APPS = [
+    'catalog.apps.CatalogConfig',
+    'user.apps.UserConfig',
+]
+
+INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
